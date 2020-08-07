f3cfdd309be5c13d3e7e23d9d947d0227acde138
Sayan Biswas <formal.sayan@gmail.com>
2020-08-07 16:10:44 +0530

    Create greet_lambda.py

---
+++ Training-and-Deploying-an-ML-Model-as-a-Microservice/greet_lambda.py
@@ -0,0 +1,23 @@
+import json
+
+print('Loading function')
+
+def lambda_handler(event, context):
+    print('Received event: ' +
+        json.dumps(event, indent=2))
+    
+    if 'greet' in event:
+        greeting = event['greet']
+    else:
+        greeting = 'Hello'
+    
+    
+    if 'name' in event:
+        name = event['name']
+    else:
+        name = 'World'
+        
+        
+    greetings = greeting + name + '!'
+    print(greetings)
+    return greetings
